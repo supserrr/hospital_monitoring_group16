c696e17635b44033d0d4dd5bbfa38c7406e01f3a
supserrr <s.shima@alustudent.com>
2024-10-19 09:55:31 -0700

    Updated archive_log.sh

--- archive_log.sh
+++ archive_log.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/env bash
-date=$(date +"%Y%m%d_%H%M%S")
-mv heart_rate_log.txt "heart_rate_log.txt_$date."
+#!/bin/bash
+# Log Archival Script
+
+generate_archive_name() {
+    echo "heart_rate_log.txt_$(date '+%Y%m%d_%H%M%S')"
+}
+
+mv "heart_rate_log.txt" $(generate_archive_name)
