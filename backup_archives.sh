f24997553e0d7ed96b8487a8c004cf60b488e271
supserrr <s.shima@alustudent.com>
2024-10-19 10:00:12 -0700

    Updated backup_archives.sh

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
 mkdir archived_logs_group16
+
 mv heart_rate_log.txt_* archived_logs_group16/
-scp -r archived_logs_group16/ d35d089e5e53@d35d089e5e53.7b3ea1d9.alu-cod.online:/home/
+
+scp -r 'd35d089e5e53@d35d089e5e53.7b3ea1d9.alu-cod.online' archived_logs_group16/:/home/
